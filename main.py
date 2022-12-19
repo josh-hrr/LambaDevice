ff7ea056b1e891e7bdfe2122a47557aa95fffead
Josué Herrera <54568984+josh-hrr@users.noreply.github.com>
2022-12-19 23:46:18 +0000

    latest

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 testingLamba = lambda number: number**2
 print(testingLamba(5))
+
+#even or odd when using lambdas
+
+testingSecond = lambda number:'YES' if number % 2 == 0 else 'NO'
+
+print(testingSecond(5))
+
+#lambda of a sum function
+
+sum = lambda n1, n2: n1 + n2
+
+print(sum(5,5))
+
+#filter
+
+myList = [1,2,3,4,5]
+
+def testing(n):
+  if n % 2 == 0:
+    return n
+
+result = list(filter(lambda n:n%2==0, myList))
+
+print(result)
